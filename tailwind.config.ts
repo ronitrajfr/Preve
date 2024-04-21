8a235325f4274a6fd121ed23687385ea3c8f0be7
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-21 22:01:38 +0530

    added file upload feat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,6 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { withUt } from "uploadthing/tw";
 
-export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+export default withUt({
+  // Your existing Tailwind config
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+});
