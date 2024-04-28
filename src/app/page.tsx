cf136892d436e919564cfca4977ad6490569009e
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-28 09:38:54 +0530

    changes made in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 import { Home } from "@/components/Home/Home";
-import { useUser } from "@clerk/clerk-react";
 
 export default function Page() {
-  const { isSignedIn, user, isLoaded } = useUser();
-
-  if (user == null || user == undefined) {
-    return;
-  }
-
   return (
     <main className="bg-slate-900 h-screen flex">
       <Home />
-      <div>Hello {user.fullName}!</div>
     </main>
   );
 }
