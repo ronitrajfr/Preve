54c49237dc07472ded9a0c0e2cd6ebd92d80a62b
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-29 19:26:25 +0530

    added things

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,61 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import pdfParse from "pdf-parse";
-import axios from "axios";
+import { readPdfText } from "pdf-text-reader";
+import axios from "axios"; // Import axios for making HTTP requests
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-  const { imageUrl, userId } = await req.json();
+  const body = await req.json();
 
   try {
-    // Fetch PDF content
-    const pdfContent = await fetchPdfContent(imageUrl);
-
-    // Store data in the database
-    await savePdfData(imageUrl, userId, pdfContent);
-
-    // Respond with success message and userId
-    return NextResponse.json({
-      message: "PDF data saved successfully",
-      userId,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json({ message: "Internal Server Error" });
-  }
-}
-
-async function fetchPdfContent(imageUrl: string) {
-  try {
-    // Fetch the PDF file using Axios
-    const response = await axios.get(imageUrl, {
-      responseType: "arraybuffer", // Ensure response type is set to arraybuffer
+    const pdfText = await readPdfText({
+      url: body.imageUrl, // Adjust the file name if needed
     });
 
-    // Parse the PDF content
-    const data = await pdfParse(response.data);
-
-    // Extract text content from the parsed data
-    const textContent = data.text;
-
-    return textContent;
+    const newPost = await prisma.post.create({
+      data: {
+        createdById: body.userId,
+        imageUrl: body.imageUrl,
+        content: pdfText, // Store the parsed text from the PDF
+      },
+    });
+    return NextResponse.json({ newPost });
   } catch (error) {
-    console.error("Error fetching PDF content:", error);
-    throw new Error("Failed to fetch PDF content");
+    console.log(error);
+    return NextResponse.json({ error });
   }
-}
-
-async function savePdfData(
-  imageUrl: string,
-  userId: string,
-  pdfContent: string
-) {
-  // Save data in the database using Prisma
-  await prisma.post.create({
-    data: {
-      imageUrl: imageUrl,
-      createdById: userId,
-      content: pdfContent,
-    },
-  });
 }
