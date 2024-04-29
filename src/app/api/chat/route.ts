0deaac32f3dbf02facdab8a85c1bfb4619199bef
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-29 15:42:31 +0530

    error encountered

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import pdf from "pdf-parse";
+import { readPdfText } from "pdf-text-reader";
 import axios from "axios"; // Import axios for making HTTP requests
 const prisma = new PrismaClient();
 
@@ -8,21 +8,18 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
-    // Fetch the PDF content from the URL
-    const response = await axios.get(body.imageUrl, {
-      responseType: "arraybuffer",
+    const pdfText = await readPdfText({
+      url: body.imageUrl, // Adjust the file name if needed
     });
-    const pdfContent = await pdf(response.data);
-
-    console.log(pdfContent.text);
 
     const newPost = await prisma.post.create({
       data: {
         createdById: body.userId,
         imageUrl: body.imageUrl,
-        content: pdfContent.text,
+        content: pdfText,
       },
     });
+    return NextResponse.json({ newPost });
   } catch (error) {
     console.log(error);
   }
