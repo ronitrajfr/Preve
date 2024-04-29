b2084c6e89b30aa4b0fc0f551d11854ddee748a3
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-29 17:30:47 +0530

    lets implement delay

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,23 +4,31 @@ import { readPdfText } from "pdf-text-reader";
 import axios from "axios"; // Import axios for making HTTP requests
 const prisma = new PrismaClient();
 
+function delay(ms: any) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
-    // const pdfText = await readPdfText({
-    //   url: body.imageUrl, // Adjust the file name if needed
-    // });
+    const pdfText = await readPdfText({
+      url: body.imageUrl, // Adjust the file name if needed
+    });
+
+    // Add a delay of 5 seconds before making the database call
+    await delay(5000);
 
     const newPost = await prisma.post.create({
       data: {
         createdById: body.userId,
         imageUrl: body.imageUrl,
-        content: "just for testing",
+        content: pdfText, // Store the parsed text from the PDF
       },
     });
     return NextResponse.json({ newPost });
   } catch (error) {
     console.log(error);
+    return NextResponse.json({ error });
   }
 }
