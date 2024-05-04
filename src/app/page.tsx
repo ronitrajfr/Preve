45d61651fe1b530816b85fefba58672f39729a6f
Ronit Raj <ronitrajofficial7@gmail.com>
2024-05-04 20:40:53 +0530

    deleted all thing

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Home } from "@/components/Home/Home";
-
-export default function Page() {
-  return (
-    <main className="bg-slate-900 h-screen flex">
-      <Home />
-    </main>
-  );
-}
--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
