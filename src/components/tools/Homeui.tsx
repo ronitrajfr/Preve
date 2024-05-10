61e8a7661749a0142b4b4636c273718c0b0c513f
Ronit Raj <ronitrajofficial7@gmail.com>
2024-05-10 21:58:57 +0530

    upgrade: updated anchor tag with NextLink

--- src/components/tools/Homeui.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Homeui = () => {
-  return <div></div>;
-};
--- src/components/tools/Navbar.tsx
+++ src/components/tools/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 export const Navbar = () => {
   return (
     <div className="flex justify-center">
       <div className="absolute z-[9999] mt-2 flex w-[calc(100vw-2rem)] flex-row items-center justify-between border border-neutral-200/75 bg-white/80 p-1 shadow-sm backdrop-blur-md md:mt-5 md:w-[48rem]">
         <div>
-          <a id="logo" href="/">
+          <Link id="logo" href="/">
             <Image src={"/preve.png"} alt="logo" width={60} height={60} />
-          </a>
+          </Link>
         </div>
         <div className="flex flex-row">
           <div className="mr-2 flex">
@@ -35,18 +36,18 @@ export const Navbar = () => {
             </a>
           </div>
           <div className="mr-0 hidden md:mr-2 md:flex">
-            <a href="/">
+            <Link href="/">
               <button className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 __className_4ece75 h-8 rounded-none border border-[#FF5F1F] bg-transparent p-3 text-xs text-[#FF5F1F] shadow-none duration-100 hover:bg-[#FF5F1F]/5 md:h-8 md:p-4 md:text-sm">
                 Home
               </button>
-            </a>
+            </Link>
           </div>
           <div className="hidden md:flex">
-            <a href="/projects">
+            <Link href="/projects">
               <button className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 __className_4ece75 h-8 rounded-none border border-[#FF5F1F] bg-[#FF5F1F] p-3 text-xs text-neutral-50 shadow-none duration-100 hover:bg-[#FF5F1F]/90 md:h-8 md:p-4 md:text-sm">
                 Projects
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
